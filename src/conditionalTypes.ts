8ff3a62525781c7328dd855aac59e8efe07503ad
Nikita <nikita_rs7@ukr.net>
2024-03-21 19:36:48 +0200

    'conditionalTypes'

--- src/conditionalTypes.ts
+++ src/conditionalTypes.ts
@@ -77,8 +77,60 @@ type P3 = Param3<fn>;
 
 // 11.
 /*
-Створіть утиліти RequiredProps<T> та OptionalProps<T>, які повертають union тип required та optional властивостей об'єкта. 
+Створіть утиліти RequiredProps<T> та OptionalProps<T>, які повертають union тип required та optional властивостей об'єкта.
 Використовуйте mapped type для перебору ключів T та conditional type для трансформації значень ключів типу T.
  Додайте загальне обмеження для T розширивши його від типу object у RequiredProps та OptionalProps.
-
 */
+
+type RequiredProps<T extends object> = {
+    [P in keyof T as T[P] extends Required<T>[P] ? P : never]: T[P];
+};
+type OptionalProps<T extends object> = {
+    [P in keyof T as T[P] extends Required<T>[P] ? never : P]: T[P];
+};
+
+// 12. Об'явіть аліас типу BookRequiredProps та BookOptionalProps, використовуючи інтерфейс Book та утиліти RequiredProps та OptionalProps. Спробуйте замість Book передати примітивний тип.
+
+type BookRequiredProps = RequiredProps<Book>;
+type BookOptionalProps = OptionalProps<Book>;
+
+type BookRequiredProps2 = RequiredProps<string>;
+type BookOptionalProps2 = OptionalProps<boolean>;
+
+// 13. Створіть утиліту RemoveProps <T extends object, TProps extends keyof T>, яка видаляє властивості TProps з переданого типу T.
+
+type RemoveProps<T extends object, TProps extends keyof T> = {
+    [P in Exclude<keyof T, TProps>]: T[P];
+};
+
+type User = {
+    name: string;
+    age: number;
+    address: string;
+};
+const removedProps: RemoveProps<User, 'name'> = {
+    age: 25,
+    address: 'unknown',
+};
+
+// 14. Об'явіть аліас типу BookRequiredPropsType та BookOptionalPropsType, використовуючи інтерфейс Book, аліаси типу BookRequiredProps та BookOptioalProps та утиліту RemoveProps Спробуйте замість Book передати Author.
+
+type Author2 = {
+    user: string;
+    age: number;
+};
+type BookRequiredPropsType = RemoveProps<Book, keyof BookOptionalProps>;
+type BookOptionalPropsType = RemoveProps<Book, keyof BookRequiredProps>;
+
+type BookRequiredPropsType2 = RemoveProps<Author2, keyof BookRequiredProps>;
+type BookOptionalPropsType2 = RemoveProps<Author2, keyof BookOptionalProps>;
+
+// 15. Створіть функцію update(), яка приймає один параметр типу boolean. Якщо значення аргументу true, функція повинна повертати значення типу string. Якщо значення аргументу false, функція повинна повертати значення типу number.
+
+const update = (param: boolean): string | number => {
+    if (param) {
+        return '';
+    } else {
+        return 1;
+    }
+};
